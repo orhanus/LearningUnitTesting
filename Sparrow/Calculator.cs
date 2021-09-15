3ab2bf9d35d61f353182170c65627bdc49fc881b
orhanus <orhanus@maestralsolutions.com>
2021-09-15 15:37:09 +0200

    Add project files.

---
+++ Sparrow/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sparrow
+{
+    public class Calculator
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ SparrowMSTest/CalculatorMSTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparrow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparrowMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            Calculator calc = new();
+            int result = calc.AddNumbers(10, 20);
+
+            Assert.AreEqual(30, result);
+        }
+    }
+}
